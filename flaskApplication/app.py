e0b6d476db9da4879b1693ba6d142f1016dae882
Tobias Kristensen <tkr048@uib.no>
2023-02-12 15:01:08 +0100

    error handel

--- flaskApplication/app.py
+++ flaskApplication/app.py
@@ -120,4 +120,5 @@ app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == "__main__":
     #from waitress import serve
+    #serve(app, host="0.0.0.0", port=8080)
     app.run(debug=True) # Should be false if it is deployed
